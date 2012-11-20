2419036683a8c39f611114c86a1db99772bd7b4b
Gustavo Granados Gutiérrez <granados.gg@gmail.com>
2012-11-20 02:10:07 -0600

    Clase para manejar solicitudes a la aplicación
    Nueva clase Request para la manipulacion de solicitudes post y get.
    
    Falta detallarla

---
+++ library/Request.php
@@ -0,0 +1,51 @@
+<?php
+
+class Request{
+
+	private $controller;
+	private $function;
+	private $arguments;
+
+	public function __contruct()
+	{
+		if (isset($_GET['url'])) {
+			$url = filter_input(INPUT_GET, 'url', FILTER_SANITIZE_URL);
+			$url = explode('/', $url);
+			$url = array_filter($url);
+
+			$this->controller = strtolower(array_shift($url));
+			$this->function = strtolower(array_shift($url));
+			$this->arguments = $url;
+		}
+
+
+		if (!$this->controller) {
+			$this->controller = DEFAULT_CONTROLLER;			
+		}
+
+		if (!$this->function) {
+			$this->controller = 'index';
+		}
+
+		if (!isset($this->arguments)) {
+			$this->arguments = array();
+		}
+
+	}
+
+
+	public function getController()
+	{
+		return $this->controller;
+	}
+
+	public function getfunction()
+	{
+		return $this->function;
+	}
+
+	public function getArguments()
+	{
+		return $this->arguments;
+	}
+}
